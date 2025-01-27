0d48e01db4e4037d7d175a176225b00ed642ea19
t-r-a-c-y <tracytesi69@gmail.com>
2025-01-27 20:56:44 +0200

    first commit

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,63 +1,25 @@
-"use client";
-import Link from "next/link";
-import React, { useState } from "react";
-import { useRouter } from "next/navigation";
-import axios from "axios";
+"use client"
+import Link from "next/link"
+import React, { useState } from "react"
+import { useRouter } from "next/navigation"
+import axios from "axios"
 
-export default function SignupPage() {
-    const [user, setUser] = useState({
-        email: "",
-        password: "",
-        username: "",
-    });
+export default function SignupPage(){
+    const [user,setUser] = useState({
+        email:"",
+        username:"",
+        password:"",
+    })
+    const onSignUp = async ()=>{
 
-    const router = useRouter();
+    }
 
-    const onSignup = async () => {
-        try {
-            const response = await axios.post("/api/signup", user);
-            console.log("Signup successful", response.data);
-            router.push("/login"); // Redirect to login page after signup
-        } catch (error) {
-            console.error("Signup failed", error);
-        }
-    };
-
-    return (
+    return(
         <div>
-            <h1>Signup</h1>
-            <form
-                onSubmit={(e) => {
-                    e.preventDefault();
-                    onSignup();
-                }}
-            >
-                <input
-                    type="text"
-                    placeholder="Username"
-                    value={user.username}
-                    onChange={(e) =>
-                        setUser({ ...user, username: e.target.value })
-                    }
-                />
-                <input
-                    type="email"
-                    placeholder="Email"
-                    value={user.email}
-                    onChange={(e) =>
-                        setUser({ ...user, email: e.target.value })
-                    }
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={user.password}
-                    onChange={(e) =>
-                        setUser({ ...user, password: e.target.value })
-                    }
-                />
-                <button type="submit">Signup</button>
-            </form>
+            <h1>Sign up</h1>
+            <hr />
+            <label htmlFor="username">username</label>
+            <input type="text" name="username" id="username" value={user.username} onChange={(e)=>setUser({...user,username:e.target.value})} />
         </div>
-    );
+    )
 }
