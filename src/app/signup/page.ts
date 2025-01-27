2aa1cb50c36c34ad067d751783f49b31f5bbba38
t-r-a-c-y <tracytesi69@gmail.com>
2025-01-27 20:36:25 +0200

    first commit

---
+++ src/app/signup/page.ts
@@ -0,0 +1,10 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
+
+export default function signupPage(){
+
+}
