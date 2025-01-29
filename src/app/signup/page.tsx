3fd068ffca12a146724f32ea8cc12dbd03009798
t-r-a-c-y <tracytesi69@gmail.com>
2025-01-29 21:17:53 +0200

    first commit

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -11,12 +11,16 @@ export default function SignupPage() {
         password: "",
     });
 
+    const onSignUp = ()=>{
+
+    }
+
     return (
         <div className="min-h-screen flex items-center justify-center">
             <div className="text-center p-6  shadow-lg rounded-lg">
                 <h1 className="text-2xl font-bold mb-4">Sign up</h1>
-                <hr className="mb-4" />
-                <div className="flex flex-col gap-4">
+              
+                <div className="flex flex-col gap-4 px-20 item-center">
                     <label htmlFor="username">Email</label>
                     <input
                         type="email"
@@ -24,6 +28,7 @@ export default function SignupPage() {
                         id="email"
                         value={user.email}
                         onChange={(e) => setUser({ ...user, email: e.target.value })}
+                        placeholder="Enter your email"
                         className="p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600 text-black"
                     />
                     <label htmlFor="username">Username</label>
@@ -33,6 +38,7 @@ export default function SignupPage() {
                         id="username"
                         value={user.username}
                         onChange={(e) => setUser({ ...user, username: e.target.value })}
+                        placeholder="Enter your username"
                         className="p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600 text-black"
                     />
                     <label htmlFor="username">Password</label>
@@ -42,8 +48,11 @@ export default function SignupPage() {
                         id="password"
                         value={user.password}
                         onChange={(e) => setUser({ ...user, password: e.target.value })}
+                        placeholder="Enter your password"
                         className="p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600 text-black"
                     />
+                    <button onClick={onSignUp} className="bg-gray-500 hover:bg-gray-700 p-3 w-32">Sign Up</button>
+                    <Link href='/login'>Already have an account? Click on the login page</Link>
                 </div>
             </div>
         </div>
