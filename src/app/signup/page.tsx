d1cbf0671938602f08f730d0502bc27767999b70
t-r-a-c-y <tracytesi69@gmail.com>
2025-01-29 21:02:50 +0200

    first commit

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -4,33 +4,30 @@ import React, { useState } from "react"
 import { useRouter } from "next/navigation"
 import axios from "axios"
 
-export default function SignupPage(){
-    const [user,setUser] = useState({
-        email:"",
-        username:"",
-        password:"",
-    })
-    const onSignUp = async ()=>{
+export default function SignupPage() {
+    const [user, setUser] = useState({
+        email: "",
+        username: "",
+        password: "",
+    });
 
-    }
-
-    return(
-        <div>
-            <h1>Sign up</h1>
-            <hr />
-            <div>
-            <label htmlFor="username">username</label>
-            <input type="text" name="username" id="username" value={user.username} onChange={(e)=>setUser({...user,username:e.target.value})}
-            className="p-2 border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" />
-
-<label htmlFor="username">username</label>
-            <input type="text" name="username" id="username" value={user.username} onChange={(e)=>setUser({...user,username:e.target.value})}
-            className="p-2 border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" />
-
-<label htmlFor="username">username</label>
-            <input type="text" name="username" id="username" value={user.username} onChange={(e)=>setUser({...user,username:e.target.value})}
-            className="p-2 border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" />
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <div className="text-center p-6  shadow-lg rounded-lg">
+                <h1 className="text-2xl font-bold mb-4">Sign up</h1>
+                <hr className="mb-4" />
+                <div className="flex flex-col gap-4">
+                    <label htmlFor="username">Username</label>
+                    <input
+                        type="text"
+                        name="username"
+                        id="username"
+                        value={user.username}
+                        onChange={(e) => setUser({ ...user, username: e.target.value })}
+                        className="p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600 text-black"
+                    />
+                </div>
             </div>
         </div>
-    )
+    );
 }
