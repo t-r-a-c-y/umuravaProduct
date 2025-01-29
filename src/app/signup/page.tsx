5dbaf6176930bed3c68e21f5e02f64219226ed22
t-r-a-c-y <tracytesi69@gmail.com>
2025-01-29 21:06:12 +0200

    first commit

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -17,6 +17,15 @@ export default function SignupPage() {
                 <h1 className="text-2xl font-bold mb-4">Sign up</h1>
                 <hr className="mb-4" />
                 <div className="flex flex-col gap-4">
+                    <label htmlFor="username">Email</label>
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        value={user.email}
+                        onChange={(e) => setUser({ ...user, email: e.target.value })}
+                        className="p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600 text-black"
+                    />
                     <label htmlFor="username">Username</label>
                     <input
                         type="text"
@@ -24,6 +33,15 @@ export default function SignupPage() {
                         id="username"
                         value={user.username}
                         onChange={(e) => setUser({ ...user, username: e.target.value })}
+                        className="p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600 text-black"
+                    />
+                    <label htmlFor="username">Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        value={user.password}
+                        onChange={(e) => setUser({ ...user, password: e.target.value })}
                         className="p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600 text-black"
                     />
                 </div>
