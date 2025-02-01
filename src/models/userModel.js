6520a6fdd5083f92ca31b540e70341410bff9449
t-r-a-c-y <tracytesi69@gmail.com>
2025-02-01 20:28:41 +0200

    first commit

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,7 +1,18 @@
 import mongoose from "mongoose";
+import { unique } from "next/dist/build/utils";
 
 const userSchema = new mongoose.Schema({
-
+    username:{
+        type:String,
+        required : [true,"Please provide the username"],
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:[true,"Please provide the email"]
+    }
 })
 
 const User = mongoose.models.users || mongoose.model("users",userSchema);
+
+export default User;
